81e02355f7a8d9655b03330dc87aff3b276914cd
Rishabh <rishabhptl2003@gmail.com>
2024-02-16 14:45:53 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+// server.js
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port 3000`);
+});
