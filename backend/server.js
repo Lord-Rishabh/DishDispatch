6f8da3d026b25a030d6699eb336ea21041cd55b6
Aayush <aayushbatham210036@acropolis.in>
2024-02-16 16:55:04 +0530

    added auth

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,24 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const connectToMongo = require('./db');
-
 connectToMongo();
 const PORT = 3000;
+app.use(express.json());
+const authRoutes = require('./routes/auth');
+
+app.use('/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Homepage')
+})
+
+app.get('/register', (req, res) => {
+  res.send('Register')
+})
+
+app.get('/login', (req, res) => {
+  res.send('Login')
+})
 
 app.listen(PORT, () => {
   console.log("Server is running on port 3000");
