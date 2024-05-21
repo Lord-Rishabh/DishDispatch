fdec343aebb551339940ecbfc6fecc91186d0451
Rishabh <rishabhptl2003@gmail.com>
2024-05-21 23:05:32 +0530

    home page redesigned

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import '../css/button_black.css';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
     const [token, setToken] = useState(null);
+    const [isOpen, setIsOpen] = useState(false);
 
+    const toggleMenu = () => {
+      setIsOpen(!isOpen);
+    };
     useEffect(() => {
         const storedToken = localStorage.getItem('token');
         setToken(storedToken);
@@ -16,33 +19,96 @@ const Header = () => {
     };
 
     return (
-        <div className='flex justify-between flex-row p-5 px-8 bg-slate-200 '>
-            <a href='/'>
-                <p className='font-semibold'>Dish Dispatch</p>
-            </a>
-            <div className='flex justify-center flex-row gap-10'>
-                <button className='font-semibold'>Dashboard</button>
-            </div>
-            {!token ? (
-                <div className='flex justify-center flex-row gap-10'>
-                    <Link to='/login'>
-                        <button className='font-semibold'>Login</button>
-                    </Link>
-                    <Link to='/register'>
-                        <button className='font-semibold text-orange-400'>Sign up</button>
-                    </Link>
-                </div>
-            ) : (
-                <div className='flex justify-center flex-row gap-10'>
-                    <Link to='/login'>
-                    <button className='font-semibold' onClick={handleLogout}>
-                        Logout
-                    </button>
-                    </Link>
-                </div>
+        // <div className='flex justify-between flex-row p-5 px-8 bg-slate-200 '>
+        //     <a href='/'>
+        //         <p className='font-semibold'>Dish Dispatch</p>
+        //     </a>
+        //     <div className='flex justify-center flex-row gap-10'>
+        //         <button className='font-semibold'>Dashboard</button>
+        //     </div>
+        //     {!token ? (
+        //         <div className='flex justify-center flex-row gap-10'>
+        //             <Link to='/login'>
+        //                 <button className='font-semibold'>Login</button>
+        //             </Link>
+        //             <Link to='/register'>
+        //                 <button className='font-semibold text-orange-400'>Sign up</button>
+        //             </Link>
+        //         </div>
+        //     ) : (
+        //         <div className='flex justify-center flex-row gap-10'>
+        //             <Link to='/login'>
+        //             <button className='font-semibold' onClick={handleLogout}>
+        //                 Logout
+        //             </button>
+        //             </Link>
+        //         </div>
 
+        //     )}
+        // </div>
+
+
+        <nav className="bg-white w-full px-8 py-3 md:px-auto">
+        <div className="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
+          <div className="flex items-center md:order-1">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
+            </svg>
+            <p className="text-xl font-bold pl-3 text-black">Dish Dispatch</p>
+          </div>
+  
+          <div className="md:hidden flex items-center">
+            <button onClick={toggleMenu} className="text-gray-500 focus:outline-none">
+              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+              </svg>
+            </button>
+          </div>
+  
+          <div className={`${isOpen ? 'block' : 'hidden'} md:flex md:items-center md:w-auto w-full order-3 md:order-2`}>
+            <ul className="flex flex-col md:flex-row font-semibold justify-between w-full md:w-auto">
+                
+              <li className="md:px-4 md:py-2 hover:text-black hover:font-bold"><Link to="/">Home</Link></li>
+              <li className="md:px-4 md:py-2 hover:text-black hover:font-bold"><Link to="/dashboard">Dashboard</Link></li>
+              <li className="md:px-4 md:py-2 hover:text-black hover:font-bold"><Link to="/">Contact</Link></li>
+            </ul>
+            <div className="md:hidden flex flex-col gap-2 mt-4">
+              {!token ? (
+                <>
+                  <Link to="/login">
+                    <button className="bg-black w-full text-center hover:bg-gray-700 hover:scale-105 ease-in-out rounded-xl text-white font-bold px-4 py-2">Login</button>
+                  </Link>
+                  <Link to="/register">
+                    <button className="text-black w-full text-center border-2 border-black hover:scale-105 ease-in-out rounded-xl font-bold px-4 py-2">Sign up</button>
+                  </Link>
+                </>
+              ) : (
+                <Link to="/login">
+                  <button className="bg-black w-full text-center hover:bg-gray-700 hover:scale-105 ease-in-out rounded-xl text-white font-bold px-4 py-2" onClick={handleLogout}>Logout</button>
+                </Link>
+              )}
+            </div>
+          </div>
+  
+          <div className="hidden md:flex order-2 md:order-3 mt-4 md:mt-0">
+            {!token ? (
+              <>
+                <Link to="/login">
+                  <button className="bg-black hover:bg-gray-700 hover:scale-105 ease-in-out rounded-xl text-white font-bold px-4 xl:px-6 py-2 xl:py-3 mr-3">Login</button>
+                </Link>
+                <Link to="/register">
+                  <button className="text-black border-2 border-black hover:scale-105 ease-in-out rounded-xl font-bold px-4 xl:px-6 py-2 xl:py-3">Sign up</button>
+                </Link>
+              </>
+            ) : (
+              <Link to="/login">
+                <button className="bg-black hover:bg-gray-700 hover:scale-105 ease-in-out rounded-xl text-white font-bold px-4 xl:px-6 py-2 xl:py-3 mr-3" onClick={handleLogout}>Logout</button>
+              </Link>
             )}
+          </div>
         </div>
+      </nav>
+
     );
 };
 
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -6,7 +6,33 @@ const Homepage = () => {
   return (
     <>  
         <Navbar/>
-        <section className='flex flex-col ml-12 mr-12'>
+        <div className="flex flex-col lg:flex-row items-center justify-between px-6  bg-white h-screen">
+        <div className="lg:w-2/3 px-12">
+          <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+            <span className="block xl:inline">Life is too short. Wear</span>{' '}
+            <span className="block text-blue-600 xl:inline">Style!</span>
+          </h1>
+          <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-lg  md:mt-5 md:text-xl lg:mx-0">
+            Fashion is part of the daily air and it changes all the time, with all the events. You can even see the approaching of a revolution in clothes. You can see and feel everything in clothes.
+          </p>
+          <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+            <div className="rounded-md shadow">
+              <Link to="/buy-now" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-black hover:bg-gray-800 md:py-4 md:text-lg md:px-10">
+                Buy Now
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="lg:w-1/2 lg:mt-0 relative">
+          <div className="absolute inset-0 rounded-full border-8 border-blue-600"></div>
+          <img
+            className="relative z-10 rounded-full w-auto h-auto scale-105"
+            src='../../public/salad.jpg'
+            alt="Fashion"
+          />
+        </div>
+      </div>
+        {/* <section className='flex flex-col ml-12 mr-12'>
           <div className='h-[70vh] flex justify-center items-center flex-col'>
           <p className='font-bold text-7xl'>
           ORDER JUST A <span className='text-orange-400'>&nbsp;SCAN&nbsp;</span>AWAY
@@ -21,7 +47,7 @@ const Homepage = () => {
           </div>
           
 
-        </section>     
+        </section>      */}
     </>
   )
 }
